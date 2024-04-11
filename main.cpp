eee8d84dc9adc4014b44739bb34faaeb30f86182
ericschmidutz <e-schmidutz@onu.edu>
2024-04-11 09:51:10 -0400

    Completed P10.1

--- main.cpp
+++ main.cpp
@@ -1,9 +1,49 @@
-#include<iostream>
+#include<iostream>""
+#include<random>
+#include<vector>
+#include<String>
 
 using namespace std;
 
-int main() {
+vector<string> sentence;
+string scramble(string word);
+string uInput;
+string word = "";
+int i = 0;
 
+int main() {
+	srand(time(0));
+
+	cout << "Enter Text: ";
+	getline(cin, uInput);
+	for (const auto& i : uInput) {
+		if (i != ' ') {
+			word += i;
+		}
+		else {
+			cout << scramble(word) << " ";
+			word = "";
+		}
+	}
 
 	return 0;
+}
+
+string scramble(string word) {
+	int stringLength = word.length();
+	int prepStringLength = stringLength - 2;
+
+	if (stringLength > 3) {
+		int randIndex1 = rand() % prepStringLength + 1;
+		int randIndex2 = rand() % prepStringLength + 1;
+		char placeHolder = word[randIndex1];
+		while (randIndex1 == randIndex2) {
+			int randIndex2 = rand() % prepStringLength + 1;
+		}
+		word[randIndex1] = word[randIndex2];
+		word[randIndex2] = placeHolder;
+		return word; 
+	}
+	
+	return word;
 }
